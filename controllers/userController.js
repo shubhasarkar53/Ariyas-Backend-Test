ec9e840eb649cac29efb2ef88f0eedc6a1b1eb0e
Shubha Sarkar <sarkarshubha123@gmail.com>
2024-08-16 15:07:02 +0530

    logout cookie option updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -152,6 +152,8 @@ exports.logoutUser = catchAsyncErr(async (req, res, next) => {
   res.cookie("token", null, {
     expires: new Date(Date.now()),
     httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'None',
   });
 
   res.status(200).json({
